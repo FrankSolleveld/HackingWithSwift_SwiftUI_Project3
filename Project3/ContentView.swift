5a62713c26f97af7f0e98524a627d1f02b53358a
Frank Solleveld <fsolleveld@outlook.com>
2021-05-24 13:40:20 +0200

    Initial Commit

---
+++ Project3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project3
+//
+//  Created by Frank Solleveld on 24/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project3/Project3App.swift
@@ -0,0 +1,17 @@
+//
+//  Project3App.swift
+//  Project3
+//
+//  Created by Frank Solleveld on 24/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Project3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
